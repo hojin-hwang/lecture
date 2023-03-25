b46335d0d2c5e5fde7a4d7039ab266360bb05961
jake <hoinh@gmail.com>
2023-03-25 16:26:43 +0900

    init;

--- src/assets/js/console-log.js
+++ src/assets/js/console-log.js
@@ -2,8 +2,6 @@ function consoleLog(arg = 'consoleLog'){
     console.log(arg);
 
     console.log("1011 commit")
-    console.log("2를 추가해")
-    console.log("3를 추가해")
 }
 
 export default consoleLog;
