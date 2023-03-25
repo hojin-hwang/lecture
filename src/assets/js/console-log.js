a9e2e09dc31a74e847f1f9af30097d1c8ceb7b7d
jake <hoinh@gmail.com>
2023-03-25 16:29:04 +0900

    1011

--- src/assets/js/console-log.js
+++ src/assets/js/console-log.js
@@ -2,10 +2,6 @@ function consoleLog(arg = 'consoleLog'){
     console.log(arg);
 
     console.log("1011 commit")
-
-    console.log("1102 commit")
-
-    console.log("1103 commit")
 }
 
 export default consoleLog;
