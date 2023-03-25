dbb29a5671123fb776e35302bd2118238618eaaf
jake <hoinh@gmail.com>
2023-03-25 16:22:55 +0900

    2를 추가해

--- src/assets/js/console-log.js
+++ src/assets/js/console-log.js
@@ -2,10 +2,7 @@ function consoleLog(arg = 'consoleLog'){
     console.log(arg);
 
     console.log("1011 commit")
-
-    console.log("1012 commit")
-
-    console.log("1013 commit")
+    console.log("2를 추가해")
 }
 
 export default consoleLog;
